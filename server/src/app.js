dbfb86602aca7e07a598c27bb1b49f517cce111f
Sujaljaiswal25 <sujaljaiswal304@gmail.com>
2025-12-22 16:39:46 +0530

    basic setup done

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Health check route
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
+
+// Routes will go here
+// app.use('/api', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+module.exports = app;
